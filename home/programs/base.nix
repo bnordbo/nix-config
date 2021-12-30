86f77367be1178ea18f7d8ba71f23cd0c1b2675e
Bjørn Nordbø <bn@strangedays.no>
2021-12-30 20:01:57 +0100

    Font manager + Python

--- home/programs/base.nix
+++ home/programs/base.nix
@@ -9,12 +9,14 @@
     pkgs.fd                             # Simple, fast and user-friendly alternative to find
     pkgs.file                           # Program that shows the type of files
     pkgs.firefox                        # Web browser built from Firefox source tree
+    pkgs.font-manager
     pkgs.git                            # Distributed version control system
     pkgs.haskellPackages.xmobar         # A Minimalistic Text Based Status Bar
     pkgs.haskellPackages.xmonad-contrib # Extra modules for the XMonad window manager
     pkgs.jq                             # Lightweight and flexible command-line JSON processor
     pkgs.kubectl                        # Kubernetes CLI
     pkgs.niv                            # Easy dependency management for Nix projects
+    pkgs.python3
     pkgs.ripgrep                        # Fast grep replacement
     pkgs.rofi                           # Window switcher, run dialog and dmenu replacement
     pkgs.xorg.xev                       # Capture X events
