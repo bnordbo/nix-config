6bd11b91a46f2ae132247ec7e45b3630b0038971
Bjørn Nordbø <bn@strangedays.no>
2022-03-13 11:51:35 +0100

    Prevent Emacs from blocking shutdown

--- home/services/emacs/default.nix
+++ home/services/emacs/default.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }:
 
 {
-  home-manager.users.bn.services.emacs = {
-    enable = true;
-#    defaultEditor = true;
-
-    client = {
+  home-manager.users.bn = {
+    services.emacs = {
       enable = true;
-      arguments = ["-c" "-n" "-q"];
+      #    defaultEditor = true;
+
+      client = {
+        enable = true;
+        arguments = ["-c" "-n" "-q"];
+      };
+    };
+
+    systemd.user.targets.emacs = {
+      Service = {
+        ExecStop = "${pkgs.emacs}/bin/emacsclient --eval \"(save-buffers-kill-terminal t)\"";
+      };
     };
   };
 }
